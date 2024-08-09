d0d9efd19617081abf0719e3ac18e316a8b2618d
hafideveloper <hafideveloper@gmail.com>
2024-08-09 14:49:31 +0300

    Initial Commit

---
+++ CollectionViewCodeExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewCodeExample
+//
+//  Created by Apple on 9.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
